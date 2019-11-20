50fded95d7162df3d6813f0b8a1472c406c9a3f4
zaqwer101 <platonov256@gmail.com>
2019-11-21 03:36:11 +0800

    Теперь с устанавливаемым COREmanager

--- billmgr/install.sh
+++ billmgr/install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 yum makecache 
-yum install wget -y
-wget http://cdn.ispsystem.com/install.sh -O /root/install.sh
-sh /root/install.sh billmanager-corporate --release beta --noinstall
-echo $TESTENV
+yum install wget psmisc -y 
+wget http://cdn.ispsystem.com/install.sh -O /root/install.sh 
+sh /root/install.sh billmanager-corporate --release beta --noinstall 
+mkdir -p /usr/local/mgr5/etc/billmgr.conf.d 
+echo -e "DBHost localhost \n \
+DBUser billmgr \n \
+DBPassword $MYSQL_PASSWORD \n \
+DBName billmgr" > /usr/local/mgr5/etc/billmgr.conf.d/db.conf
+yum install -y coremanager
+killall -9 ihttpd 
+/usr/local/mgr5/sbin/mgrctl -m core session.newkey key=sdkfmnsdbf6dt8s7yf98udsiofjsdh
+/usr/local/mgr5/sbin/ihttpd -d 
