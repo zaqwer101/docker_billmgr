05a87cb7f82137f01bf3ac3ecc4594d6ab13ea97
zaqwer101 <platonov256@gmail.com>
2019-11-22 05:05:00 +0800

    Расширенное издание Теперь ставится из образа с предустановленными зависимостями

--- billmgr/install.sh
+++ billmgr/install.sh
@@ -4,24 +4,18 @@ echo $MYSQL_SERVER_IP
 echo "Начинаем.."
 if [ ! -f /root/.installed ]; then
     echo "Панель не установлена"
-    echo "root:$BILLMGR_PASSWORD" | chpasswd
-    yum install wget psmisc -y 
-    wget http://cdn.ispsystem.com/install.sh -O /root/install.sh 
-    sh /root/install.sh billmanager-corporate --release beta --noinstall 
     mkdir -p /usr/local/mgr5/etc/billmgr.conf.d
-
     echo -e "DBHost 172.30.20.101 \n\
 DBUser billmgr \n\
 DBPassword 944sn3O8d92s \n\
 DBName billmgr" > /usr/local/mgr5/etc/billmgr.conf.d/db.conf
 
-    CONFIG=$(cat /usr/local/mgr5/etc/billmgr.conf | grep -v "DB")
+#   CONFIG=$(cat /usr/local/mgr5/etc/billmgr.conf | grep -v "DB")
 
-    echo -e "DBHost 172.30.20.101 \n\
-DBUser billmgr \n\
-DBPassword 944sn3O8d92s \n\
-DBName billmgr \n\
-$CONFIG\n" > /usr/local/mgr5/etc/billmgr.conf
+    echo -e "DBHost 172.30.20.101\n\
+DBUser billmgr\n\
+DBPassword 944sn3O8d92s\n\
+DBName billmgr\n" > /usr/local/mgr5/etc/billmgr.conf
     yum install -y coremanager
     killall -9 ihttpd
     echo -e " \
@@ -35,8 +29,8 @@ else
     echo "Панель уже установлена"
 fi
 
-
 cat /usr/local/mgr5/etc/ihttpd.conf
 /usr/local/mgr5/sbin/mgrctl -m billmgr session.newkey key=sdkfmnsdbf6dt8s7yf98udsiofjsdh
+/usr/local/mgr5/sbin/mgrctl -m billmgr employee.edit elid=1 password=$BILLMGR_PASSWORD confirm=$BILLMGR_PASSWORD
 killall -9 ihttpd 
 /usr/local/mgr5/sbin/ihttpd -d 
