8972a082506dbf3d226ae15456a55c509f0fbf95
zaqwer101 <platonov256@gmail.com>
2019-11-21 21:54:10 +0800

    Теперь со статичными адресами для адресации внутри контейнеров

--- billmgr/install.sh
+++ billmgr/install.sh
@@ -5,28 +5,28 @@ wget http://cdn.ispsystem.com/install.sh -O /root/install.sh
 sh /root/install.sh billmanager-corporate --release beta --noinstall 
 mkdir -p /usr/local/mgr5/etc/billmgr.conf.d 
 echo -e " \
-    DBHost mysql-service \n \
-    DBUser billmgr \n \
-    DBPassword $MYSQL_PASSWORD \n \
-    DBName billmgr" > /usr/local/mgr5/etc/billmgr.conf.d/db.conf
+DBHost 172.20.20.101 \n \
+DBUser billmgr \n \
+DBPassword $MYSQL_PASSWORD \n \
+DBName billmgr" > /usr/local/mgr5/etc/billmgr.conf.d/db.conf
 
 CONFIG=$(cat /usr/local/mgr5/etc/billmgr.conf | grep -v "DB")
 
 echo -e " \
-    $CONFIG \n \
-    DBHost mysql-service \n \
-    DBUser billmgr \n \
-    DBPassword $MYSQL_PASSWORD \n \
-    DBName billmgr" > /usr/local/mgr5/etc/billmgr.conf
-
+$CONFIG \n \
+DBHost 172.20.20.101 \n \
+DBUser billmgr \n \
+DBPassword $MYSQL_PASSWORD \n \
+DBName billmgr" > /usr/local/mgr5/etc/billmgr.conf
+yum install -y coremanager
+killall -9 ihttpd
 echo -e " \
     listen { \n \
         ip * \n \
-        redirect \n \
-}" > /usr/local/mgr5/etc/ihttpd.conf
-
-yum install -y coremanager
-killall -9 ihttpd 
+        port 1500 \n \
+        redirect }" > /usr/local/mgr5/etc/ihttpd.conf
 yum install -y billmanager-corporate
+cat /usr/local/mgr5/etc/ihttpd.conf
 /usr/local/mgr5/sbin/mgrctl -m billmgr session.newkey key=sdkfmnsdbf6dt8s7yf98udsiofjsdh
+killall -9 ihttpd 
 /usr/local/mgr5/sbin/ihttpd -d 
