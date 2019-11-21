aebdb509b7b53fc5f5e1744b19a4c2ca93de2e11
zaqwer101 <platonov256@gmail.com>
2019-11-21 17:43:57 +0800

    BILLmanager иногда поднимается

--- billmgr/install.sh
+++ billmgr/install.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
-yum makecache 
 yum install wget psmisc -y 
 wget http://cdn.ispsystem.com/install.sh -O /root/install.sh 
 sh /root/install.sh billmanager-corporate --release beta --noinstall 
 mkdir -p /usr/local/mgr5/etc/billmgr.conf.d 
-echo -e "DBHost localhost \n \
-DBUser billmgr \n \
-DBPassword $MYSQL_PASSWORD \n \
-DBName billmgr" > /usr/local/mgr5/etc/billmgr.conf.d/db.conf
+echo -e " \
+    DBHost mysql-service \n \
+    DBUser billmgr \n \
+    DBPassword $MYSQL_PASSWORD \n \
+    DBName billmgr" > /usr/local/mgr5/etc/billmgr.conf.d/db.conf
+
+CONFIG=$(cat /usr/local/mgr5/etc/billmgr.conf | grep -v "DB")
+
+echo -e " \
+    $CONFIG \n \
+    DBHost mysql-service \n \
+    DBUser billmgr \n \
+    DBPassword $MYSQL_PASSWORD \n \
+    DBName billmgr" > /usr/local/mgr5/etc/billmgr.conf
+
+echo -e " \
+    listen { \n \
+        ip * \n \
+        redirect \n \
+}" > /usr/local/mgr5/etc/ihttpd.conf
+
 yum install -y coremanager
 killall -9 ihttpd 
-/usr/local/mgr5/sbin/mgrctl -m core session.newkey key=sdkfmnsdbf6dt8s7yf98udsiofjsdh
+yum install -y billmanager-corporate
+/usr/local/mgr5/sbin/mgrctl -m billmgr session.newkey key=sdkfmnsdbf6dt8s7yf98udsiofjsdh
 /usr/local/mgr5/sbin/ihttpd -d 
